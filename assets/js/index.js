8761f2fe69b3743a5da73d3a40effdba0c705f28
VictoriaAcunaParedes <victoria.acuna2709@gmail.com>
2024-03-12 21:29:37 -0300

    init

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+//navbar smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//activacion tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
